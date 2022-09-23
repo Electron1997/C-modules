42945311df701e965dbde0d6ac66ec07d570188f
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-09-23 22:43:24 +0200

    Added check()

--- utils_test.c
+++ utils_test.c
@@ -1,13 +1,18 @@
 #include <assert.h>
-#include "utils.h"
+
+#include "module.h"
 
 int main(int argc, char *argv[]){
-    
-    int a = 5, b = 2;
+
+    ull a = 5, b = 2;
     
     // Test swap()
     swap(&a, &b);
     assert(a == 2 && b == 5);
+    
+    // Test check()
+    assert(!check(0.1, 0.2, 0.05));
+    assert(check(4.9, 4.8999999, 0.00001));
     
     return 0;
 }
