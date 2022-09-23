acfe54e472940bf500fe067bb197dcc7d46ac2a8
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-09-23 22:46:25 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,13 @@
+#include <math.h>
+
 #include "utils.h"
 
-void swap(int *a, int *b){
+inline void swap(int *a, int *b){
 	int t = *a;
 	*a = *b;
 	*b = t;
+}
+
+inline int check(double a, double b, double eps){
+	return fabs(a - b) < eps;
 }
