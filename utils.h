d4b9a5a3cfce34a6be479d5dc1b80582b46bae64
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-09-23 22:45:36 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,6 +1,11 @@
 #ifndef CUSTOM_UTILS
 #define CUSTOM_UTILS
 
+typedef long long ll;
+typedef unsigned long long ull;
+
 void swap(int *a, int *b);
+
+int check(double a, double b, double eps);
 
 #endif
