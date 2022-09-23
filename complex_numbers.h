35bdd9605c55fcbddd418e904d5813a5b58b262e
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-09-23 23:25:37 +0200

    Added PI

--- complex_numbers.h
+++ complex_numbers.h
@@ -16,49 +16,43 @@ struct polar_form {
 typedef struct cartesian_form complex;
 typedef struct polar_form polar;
 
-
 // Conversions
 
-inline double abs(complex a);
+double mod(complex a);
 
-inline double arg(complex a);	// in [0, 2 * PI[
+double arg(complex a);	// in [0, 2 * PI[
 
-inline double re(polar a);
+double re(polar a);
 
-inline double im(polar a);
+double im(polar a);
 
-inline complex to_cartesian(polar a);
+complex to_cartesian(polar a);
 
-inline polar to_polar(complex a);
-
+polar to_polar(complex a);
 
 // Unary operations on cartisian form
 
-inline complex conj(complex a);
-
+complex conj(complex a);
 
 // Binary operations on cartesian form
 
-inline complex add(complex a, complex b);
+complex add(complex a, complex b);
 
-inline complex sub(complex a, complex b);
+complex sub(complex a, complex b);
 
-inline complex mul(complex a, complex b);
+complex mul(complex a, complex b);
 
-inline complex div(complex a, complex b);
-
+complex div(complex a, complex b);
 
 // Unary operations on polar form
 
-inline polar p_conj(polar a);
-
+polar p_conj(polar a);
 
 // Binary operations on polar form
 
-inline polar p_mul(polar a, polar b);
+polar p_mul(polar a, polar b);
 
-inline polar p_div(polar a, polar b);
-
+polar p_div(polar a, polar b);
 
 // Might add roots and logarithms
 
