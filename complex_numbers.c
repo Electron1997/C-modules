372343e8641a1b21d8bda8495f52ca20d95f411a
Mirco Paul <63196848+Electron1997@users.noreply.github.com>
2022-09-23 23:25:06 +0200

    Implemented arg()

--- complex_numbers.c
+++ complex_numbers.c
@@ -1,67 +1,86 @@
 #include <math.h>
+
 #include "complex_numbers.h"
 
 // Conversions
 
-double abs(complex a){
+inline double mod(complex a){
 	return sqrt(a.re * a.re + a.im * a.im);
 }
 
-double arg(complex a){	
-	return arctan(a.im / a.re);	// in [0, 2 * PI[
+inline double arg(complex a){	// result in [0, 2 * PI[, returns 0 for 0 + 0i
+	if(a.re == 0.0){
+		if(a.im >= 0.0){
+			return PI / 2;
+		}else{
+			return 3 * PI / 2;
+		}
+	}
+	double phi = atan(a.im / a.re);
+	if(a.re < 0.0){
+		phi += PI;
+	}
+	if(phi < 0.0){
+		phi += 2 * PI;
+	}
+	return phi;
 }
 
-double re(polar a){
+inline double re(polar a){
 	return a.abs * cos(a.arg);
 }
 
-double im(polar a){
+inline double im(polar a){
 	return a.abs * sin(a.arg);
 }
 
-complex to_cartesian(polar a);
+inline complex to_cartesian(polar a){
+	return (complex) {re(a), im(a)};
+}
 
-polar to_polar(complex a);
-
+inline polar to_polar(complex a){
+	return (polar) {mod(a), arg(a)};
+}
 
 // Unary operations on cartisian form
 
-complex conj(complex a){
+inline complex conj(complex a){
 	return (complex) {a.re, - a.im};
 }
 
-
 // Binary operations on cartesian form
 
-complex add(complex a, complex b){
+inline complex add(complex a, complex b){
 	return (complex) {a.re + b.re, a.im + b.im};
 }
 
-complex sub(complex a, complex b){
+inline complex sub(complex a, complex b){
 	return (complex) {a.re - b.re, a.im - b.im};
 }
 
-complex mul(complex a, complex b){
+inline complex mul(complex a, complex b){
 	return (complex) {a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re};
 }
 
-complex div(complex a, complex b);
-
+inline complex div(complex a, complex b){
+	complex c = conj(b);
+	double den = mod(c);
+	complex num = mul(a, c);
+	return (complex) {num.re / den, num.im / den};
+}
 
 // Unary operations on polar form
 
-polar p_conj(polar a){
+inline polar p_conj(polar a){
 	return (polar) {a.abs, - a.arg};
 }
 
-
 // Binary operations on polar form
 
-polar p_mul(polar a, polar b){
+inline polar p_mul(polar a, polar b){
 	return (polar) {a.abs * b.abs, a.arg + b.arg};
 }
 
-polar p_div(polar a, polar b){
+inline polar p_div(polar a, polar b){
 	return (polar) {a.abs / b.abs, a.arg - b.arg};
 }
-
